8c4f795b5a5bfccaebfe0a5871e8a4d3bf9bf67c
Phyoe Thuta <sansanphyo31@gmail.com>
2023-12-20 18:03:26 +0700

    python

---
+++ ptt.py
@@ -0,0 +1,70 @@
+print("hello" ,end='')
+print('world')
+print("\"Phyoe\"\"thuta\"")
+
+print("Phyoe\nThuta")
+print("Phyoe")
+print("\tThuta")
+print("\t\tPtt")
+
+name ="Phyoe Thuta"
+place ="Yangon"
+age = 23
+age2= 27
+print(f"My name is {name} , I live in {place} and {age} years old.")
+
+
+need = "AWS Certificate"
+need2 = "AWS Certificates"
+
+print("I need a %s" %need)
+print("I need %d %s" %(5,need2))
+
+print("I need a {}".format(need))
+print("I need %d {}".format(need2)%(5))
+
+print(f"My name is {name},I live in {place} and {age} years old.")
+Total_Tax = 24.55
+print(age)
+print(type(age))
+print(type(Total_Tax))
+print(age > age2)
+print(age < age2)
+print(age >= age2)
+print(age2 >= age)
+print(age == age2)
+print(age != age2)
+
+first = [1,2,3]
+second = [1,2,3]
+print(id(first))
+print(id(second))
+print(first is second)
+print(first is not second)
+
+
+a = "ccna"
+b = "gello" 
+print(a > b)
+print(b > a )
+
+print(ord("c"))
+print(ord("g"))
+
+
+a = 10 
+b = 3 
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+program = 'ccna cisco certified network associate'
+program2 = ' AWS'
+length = len(program)
+result = program.upper()
+print(result)
+
+print(program is not program2)
